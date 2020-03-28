33033d32d95119fbf572adaec0ab942173b6d2d4
Thibault Moulin <t.moulin76@gmail.com>
2020-03-28 11:51:52 +0100

    Music - Complete logic to fetch asynchronously musician tabs data

--- src/app/music/music.component.ts
+++ src/app/music/music.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import {IAppState} from '../core/store/App/App.state';
 import {Store} from '@ngrx/store';
-import {Observable} from 'rxjs';
-import {Instrument, Music, User} from '../core/models';
+import {merge, Observable} from 'rxjs';
+import { Instrument, Music, Musician, User } from '../core/models';
 import {selectFocusedMusic} from '../core/store/music/music.selectors';
 import {selectInstrumentList} from '../core/store/instrument/instrument.selectors';
+import { tap } from 'rxjs/operators';
 
 export interface MusicianTab {
-  tabTitle: string;
-  instrument: Instrument;
+  headerLabel?: string;
+  instrument: string | Instrument;
   user: User;
 }
 
@@ -25,24 +26,74 @@ export class MusicComponent implements OnInit {
   instruments: Instrument[] = [];
   musicianTabs: MusicianTab[] = [];
 
+  private static byTabTitle = (a: MusicianTab, b: MusicianTab) => a.headerLabel.localeCompare(b.headerLabel);
+
   constructor(private store: Store<IAppState>) {
     this.focusedMusicState$ = this.store.select(selectFocusedMusic);
     this.instrumentsListState$ = this.store.select(selectInstrumentList);
   }
 
   ngOnInit() {
-    this.focusedMusicState$.subscribe(focusedMusic => this.music = focusedMusic);
-    this.instrumentsListState$.subscribe(instrumentList => this.instruments = instrumentList);
-
-    this.drawMusicianTabs();
+    merge(
+      this.focusedMusicState$,
+      this.instrumentsListState$,
+    ).pipe(
+      tap((result: Music | Instrument[]) => {
+        if (result instanceof Music) {
+          this.musicianTabs = [];
+          this.music = result;
+        }
+        if (result instanceof Array) {
+          this.instruments = result;
+        }
+      })
+    ).subscribe(() => this.fillMusicianTabs());
   }
 
-  private drawMusicianTabs() {
+  private fillMusicianTabs() {
     if (this.music) {
-      for (const musician of this.music.musicians) {
-        // TODO Draw musician tabs
+      for (const musician of (this.music.musicians as Musician[])) {
+        for (const mInstrument of (musician as Musician).instruments) {
+          const musicianTabsLength =  this.musicianTabs.push({
+            instrument: mInstrument,
+            user: musician.user as User,
+          });
+          this.updateTabsTitle(musicianTabsLength - 1);
+        }
       }
+      this.musicianTabs.sort(MusicComponent.byTabTitle);
     }
+  }
+
+  private updateTabsTitle(newEntryPosition: number) {
+    const newEntry = this.musicianTabs[newEntryPosition];
+
+    const nestedInstrument: Instrument = this.instruments.find(
+      instrument => instrument['@id'] === (newEntry.instrument['@id'] || newEntry.instrument)
+    );
+    const tabsEqualInstrument = this.musicianTabs.filter(
+      tab => tab.instrument === (nestedInstrument['@id'] || nestedInstrument)
+    );
+
+    if (tabsEqualInstrument.length > 1) {
+      this.patchTabsTitle(tabsEqualInstrument, nestedInstrument);
+    } else {
+      newEntry.headerLabel = nestedInstrument.name;
+    }
+  }
+  /**
+   * Number the tabs with the same label to better distinguish them
+   *
+   * @param { Musician[] } toRetitle Tabs with the same instrument which need to be retitled
+   * @param { Instrument } instrument Entity which gives its name to the tabs
+   */
+  private patchTabsTitle(toRetitle: MusicianTab[], instrument: Instrument) {
+    let theSameTotal = toRetitle.length;
+    this.musicianTabs.forEach(tab => {
+      if (toRetitle.includes(tab)) {
+        tab.headerLabel = `${instrument.name} ${theSameTotal--}`;
+      }
+    });
   }
 
 }
