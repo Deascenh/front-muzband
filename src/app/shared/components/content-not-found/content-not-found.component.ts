ae5ea4b727d27059788b1f9e3ddd4668002f697c
Deascenh <t.moulin76@gmail.com>
2020-04-07 20:15:10 +0200

    404 - Re-adjust some size

--- src/app/shared/components/content-not-found/content-not-found.component.ts
+++ src/app/shared/components/content-not-found/content-not-found.component.ts
@@ -1,15 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-content-not-found',
   templateUrl: './content-not-found.component.html',
   styleUrls: ['./content-not-found.component.scss']
 })
-export class ContentNotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class ContentNotFoundComponent { }
