9c99d5ad80c8f465c889469580311ca7f49776c2
Deascenh <t.moulin76@gmail.com>
2020-05-11 16:56:27 +0200

    App - Udpated prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import {EnvironmentInterface} from './environment.interface';
 
 export const environment: EnvironmentInterface = {
   production: true,
-  paired_api_base_url: '',
-  jwt_whitelisted_domains: [],
+  paired_api_base_url: 'https://muzband-api.deascenh.fr/api/',
+  jwt_whitelisted_domains: ['muzband-api.deascenh.fr'],
   jwt_blacklisted_domains: [],
 };
